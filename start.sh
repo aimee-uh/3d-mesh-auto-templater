5062621560a9663a9a46879b426cb955c8b50f75
Aimee Bowen <bowen26@hawaii.edu>
2022-05-10 16:28:45 -1000

    added execute privileges to certain files

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ python manage.py makemigrations gui
 python manage.py migrate gui
 echo collecting static files
 python manage.py collectstatic --noinput
+echo applying execute permissions to necessary files
+chmod +x APPDIST/ganger/build/Deformation
+chmod +x APPDIST/ply2asc
 echo starting the app with gunicorn
 gunicorn --timeout 3600 gui.wsgi -w 4 \
     --bind 0.0.0.0:8000
